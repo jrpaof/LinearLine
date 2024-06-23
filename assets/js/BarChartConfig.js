a426426a7f18960cecea220bfe90b70a1a6a8424
jrpaof <91465160+jrpaof@users.noreply.github.com>
2024-06-23 20:17:31 +0700

    Add files via upload

---
+++ assets/js/BarChartConfig.js
@@ -0,0 +1,23 @@
+/*$(document).ready(function() {
+chart1 = document.querySelector('#graph1 canvas').chart
+updateConfigObject(chart1);  
+console.log(chart1.options); 
+function updateConfigObject(chart) {
+chart.options = {
+       maintainAspectRatio:true,
+    title: {
+        display: true,
+    },
+    responsive: true,
+    scales: {
+        xAxes: [{
+            stacked: true,
+        }],
+        yAxes: [{
+            stacked: true,
+        }]
+    }
+};
+chart.update();
+}
+ });*/
